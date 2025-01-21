ce5723a1957d4c3358f1340408d674652c26ba23
Piotr-Trzcinski <cinekenic@go2.pl>
2025-01-21 22:56:45 +0100

    6.2 calculator testing module added

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public class MathOperations {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
