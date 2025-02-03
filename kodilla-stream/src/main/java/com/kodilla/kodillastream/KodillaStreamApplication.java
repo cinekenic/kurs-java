d9e00b15afd89ca2a3712732e6feace37fa46312
Piotr-Trzcinski <cinekenic@go2.pl>
2025-02-03 14:20:20 +0100

    7.1 text modification

---
+++ kodilla-stream/src/main/java/com/kodilla/kodillastream/KodillaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaStreamApplication.class, args);
+	}
+
+}
