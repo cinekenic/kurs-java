539056ac8b88c564a9da1e6d7d46420a30789fca
Piotr-Trzcinski <cinekenic@go2.pl>
2025-03-05 16:42:33 +0100

    ForumUser

--- kodilla-exception/src/main/java/com/kodilla/kodillaexception/KodillaExceptionApplication.java
+++ kodilla-exception/src/main/java/com/kodilla/kodillaexception/KodillaExceptionApplication.java
@@ -1,13 +1,11 @@
-package com.kodilla.kodillaexception;
+package com.kodilla.kodillaexception; // Upewnij się, że pasuje do struktury katalogów
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaExceptionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KodillaExceptionApplication.class, args);
 	}
-
 }
